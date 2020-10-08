c242e530395b3067f0196987e2b0f42b6feecbcf
QingQing Dou <qqdou@thoughtworks.com>
2020-10-08 12:11:48 +0800

    clean priject
    Signed-off-by: QingQing Dou <qqdou@thoughtworks.com>

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/12', function(req, res, next) {
+router.get('/register', function(req, res, next) {
   res.send('respond with a resource');
 });
 
---
+++ test/users.js
@@ -0,0 +1,8 @@
+var assert = require('assert');
+describe('mocha test demo', function () {
+    describe('demo', function () {
+        it('should be true', function () {
+            assert.equal(1, 1);
+        });
+    });
+});
